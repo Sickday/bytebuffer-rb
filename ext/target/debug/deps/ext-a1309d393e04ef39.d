d8b9acf107087469442abd343e8d3474cc59fb5c
Patrick W. <Sickday@pm.me>
2024-07-12 19:48:02 -0400

    feat: add rake tasks

--- ext/target/debug/deps/ext-a1309d393e04ef39.d
+++
@@ -1,9 +0,0 @@
-/Users/sickday/Projects/Contrib/bytebuffer-rs/ext/target/debug/deps/libext-a1309d393e04ef39.rmeta: src/lib.rs Cargo.toml
-
-/Users/sickday/Projects/Contrib/bytebuffer-rs/ext/target/debug/deps/ext-a1309d393e04ef39.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
